cd94f479f2776619cbc53eb7ffd5f2c4963e054e
Yaniv Kessler <yanivk@gmail.com>
2014-02-02 18:20:07 +0200

    zz

--- axon/client.js
+++ axon/client.js
@@ -3,6 +3,10 @@ var sock = axon.socket('req')
 
 sock.bind('tcp://127.0.0.1:' + process.argv[2])
 
+sock.on('bind', function() {
+	console.log('bound')
+})
+
 var received = 0
 var sent = 0
 function onRes(res){
@@ -21,4 +25,5 @@ setInterval(function () {
 setTimeout(function () {
 	console.log('client-s: %d m/sec', sent / 20)
 	console.log('client-r: %d m/sec', received / 20)
+	process.exit(0)
 }, 20000)
--- axon/server.js
+++ axon/server.js
@@ -3,6 +3,11 @@ var sock = axon.socket('rep')
 
 sock.connect('tcp://127.0.0.1:' + process.argv[2])
 
+sock.on('connect', function() {
+	console.log('connected')
+})
+
+
 var received = 0
 
 sock.on('message', function(img, reply){
@@ -13,5 +18,5 @@ sock.on('message', function(img, reply){
 setTimeout(function () {
 
 	console.log('server-r: %d m/sec', received)
-
+	process.exit(0)
 }, 20000)
